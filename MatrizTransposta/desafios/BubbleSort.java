3d87e52c9192716f1ce086f05afbcb1aeddea82d
JamilsonPereira <jamilson.nunesp@hotmail.com>
2022-02-12 21:08:37 -0300

    feat: Bubble sort

---
+++ MatrizTransposta/desafios/BubbleSort.java
@@ -0,0 +1,21 @@
+package MatrizTransposta;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int[]v = {55,35,45,70};
+
+    }
+    private static void bolha(int[] v){
+        for(int ultimo = v.length-1; ultimo > 0; ultimo--){
+            for(int i = 0;  i < ultimo; i++){
+                if ((v[i] > v[i+1]))
+                    trocar(v, i, i+1);
+            }
+        }
+    }
+    public static void trocar(int[]v, int i, int j){
+        int aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+}
